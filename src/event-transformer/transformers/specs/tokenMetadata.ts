b42b07152c378151bcc722eee73e3102d1087a93
Noah Prince <83885631+ChewingGlass@users.noreply.github.com>
2022-01-27 10:56:37 -0600

    Feature/mpl v2 (#10)
    * Complete rewrite of the prior half of the pipeline to use getConfirmedSignatures
    
    * Fixes
    
    * Add name events and es
    
    * Implement opt out message bot
    
    * Fix to use generic handle to avoid opting on messaged from self
    
    * Switch ordering of start slot
    
    * Better handling under load
    
    * Bugfixes and delete zero values
    
    * better message
    
    * Metaplex v2
    
    * Bump web3
    
    * Bump versions

--- src/event-transformer/transformers/specs/tokenMetadata.ts
+++ src/event-transformer/transformers/specs/tokenMetadata.ts
@@ -1,10 +1,15 @@
-import { METADATA_SCHEMA } from "@strata-foundation/spl-utils";
+import { CreateMetadataArgs, UpdateMetadataArgs, CreateMetadataV2Args, UpdateMetadataV2Args, MetadataData } from "@metaplex-foundation/mpl-token-metadata";
 
-const schemaKeys =  [...METADATA_SCHEMA.keys()];
 
 export default {
   programId: "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s",
-  schema:  METADATA_SCHEMA,
+  schema: new Map([
+    ...MetadataData.SCHEMA,
+    ...CreateMetadataV2Args.SCHEMA,
+    ...CreateMetadataArgs.SCHEMA,
+    ...UpdateMetadataV2Args.SCHEMA,
+    ...UpdateMetadataArgs.SCHEMA
+  ]),
   commands: [{
     name: "CreateMetadata",
     index: 0,
@@ -15,14 +20,33 @@ export default {
       "payer",
       "updateAuthority"
     ],
-    args: schemaKeys[0]
-  },{
+    args: CreateMetadataArgs
+  }, {
     name: "UpdateMetadata",
     index: 1,
     accounts: [
       "tokenMetadata",
       "updateAuthority",
     ],
-    args: schemaKeys[1]
+    args: UpdateMetadataArgs
+  }, {
+    name: "CreateMetadataV2",
+    index: 16,
+    accounts: [
+      "tokenMetadata",
+      "mint",
+      "mintAuthority",
+      "payer",
+      "updateAuthority"
+    ],
+    args: CreateMetadataV2Args
+  }, {
+    name: "UpdateMetadataV2",
+    index: 15,
+    accounts: [
+      "tokenMetadata",
+      "updateAuthority",
+    ],
+    args: UpdateMetadataV2Args
   }]
 }
